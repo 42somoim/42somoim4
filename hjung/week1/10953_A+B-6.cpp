84d860bc78194ef98b83061286a6ca56b997f671
hysimok <jeonghy95@gmail.com>
2020-10-18 23:43:07 +0900

    A+B

--- hjung/week1/10953_A+B-6.cpp
+++ hjung/week1/10953_A+B-6.cpp
@@ -2,11 +2,15 @@
 
 int main()
 {
-		int		a, b;
+		int		a, b, i, T;
 		char	c;
-
-		std::cin >> a >> c >> b;
-		std::cout << a + b << std::endl;
+		
+		std::cin >> T;
+		for (i = 0; i < T; i++)
+		{
+				std::cin >> a >> c >> b;
+				std::cout << a + b << std::endl;
+		}
 		
 		return (0);
 }
