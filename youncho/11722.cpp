7895010ffdb006f8fabd350f611ff94949c66877
Younghyo Cho <yhcho0405@kakao.com>
2020-10-22 19:43:34 +0900

    Create 11722.cpp

---
+++ youncho/11722.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n, arr[1002], dp[1002];
+int main() {
+	cin >> n;
+	for(int i = 1; i <= n; i++) {
+		cin >> arr[i]; dp[i] = 1;
+		for(int j = i; j > 0; j--) if(arr[j] > arr[i]) dp[i] = max(dp[i], dp[j] + 1);
+	}
+	sort(dp, dp + n + 1);
+	cout << dp[n];
+}
